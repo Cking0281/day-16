abd5d044797f9b415ab03ee6e61f32d31f0e94a1
Cody King <cody.king04@outlook.com>
2022-06-29 11:20:44 -0400

    Finished tutorial

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from turtle import Turtle, Screen
+from prettytable import PrettyTable
+
+
 def main():
-    pass
+    # timmy = Turtle()
+    # print(timmy)
+    # timmy.shape("turtle")
+    # timmy.color("coral")
+    # timmy.forward(100)
+    #
+    # my_screen = Screen()
+    # print(my_screen.canvheight)
+    # my_screen.exitonclick()
+    table = PrettyTable()
+    table.align = "l"
+    table.field_names = ["Pokemon Name", "Type"]
+    table.add_row(["Pikachu", "Electric"])
+    table.add_row(["Squirtle", "Water"])
+    table.add_row(["Charmander", "Fire"])
+    print(table)
 
 
 if __name__ == "__main__":
